988296651485685e463e9e29b986d7ee04e373a3
Tom Beynon <tombeynon@gmail.com>
2015-09-17 22:55:13 +0100

    Require component classes in dummy

--- test/dummy/config/initializers/mountain_view.rb
+++ test/dummy/config/initializers/mountain_view.rb
@@ -1,3 +1,6 @@
 MountainView.configure do |config|
   config.included_stylesheets = ["global"]
 end
+
+# include component classes
+Dir.glob('app/components/**/*.rb') { |f| require_relative "../../#{f}" }
