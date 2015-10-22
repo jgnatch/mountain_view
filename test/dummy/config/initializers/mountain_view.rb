00f75d6b8dc3b06a7c39a8e7b1f71ee6a8916cc8
Tom Beynon <tombeynon@gmail.com>
2015-10-22 20:55:42 +0100

    Handle autoloading in engine initializer

--- test/dummy/config/initializers/mountain_view.rb
+++ test/dummy/config/initializers/mountain_view.rb
@@ -1,6 +1,3 @@
 MountainView.configure do |config|
   config.included_stylesheets = ["global"]
 end
-
-# include component classes
-Dir.glob('app/components/**/*.rb') { |f| require_relative "../../#{f}" }
