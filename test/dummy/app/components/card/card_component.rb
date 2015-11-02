692f606519def7c64a22def969dac4390a6104f8
Tom Beynon <tombeynon@gmail.com>
2015-11-02 11:10:23 +0000

    Update CardComponent

--- test/dummy/app/components/card/card_component.rb
+++ test/dummy/app/components/card/card_component.rb
@@ -1,7 +1,7 @@
 class CardComponent < MountainView::Presenter
-  attributes :title, :description, :link, :image_url
-  attribute :data, default: []
-  attribute :foobar
+  properties :title, :description, :link, :image_url
+  property :data, default: []
+  property :foobar
 
   def title
     ["Foobar", properties[:title]].join(" - ")
