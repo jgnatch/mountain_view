6036a8d7c7c196f064c0e8f5b946656adcf1edf2
Tom Beynon <tombeynon@gmail.com>
2016-04-14 16:59:48 -0400

    Some actual presenter tests Not sure what this was doing before..

--- test/mountain_view/presenter_test.rb
+++ test/mountain_view/presenter_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+
 class InheritedPresenter < MountainView::Presenter
   properties :title, :description
   property :data, default: []
@@ -8,13 +9,30 @@ class InheritedPresenter < MountainView::Presenter
   end
 end
 
-class MountainViewPresenterTest < ActiveSupport::TestCase
-  def test_partial
+class MountainView::PresenterTest < ActiveSupport::TestCase
+  test 'returns the correct partial path' do
     presenter = MountainView::Presenter.new("header")
     assert_equal "header/header", presenter.partial
   end
-
-  def add_properties(properties)
-    properties.merge(properties: properties)
+  
+  test 'exposes properties as provided' do
+    properties = {:foo => 'bar', :hello => 'world'}
+    presenter = MountainView::Presenter.new("header", properties)
+    assert_equal properties, presenter.properties
+  end
+  
+  test 'inherited presenter returns the correct title' do
+    presenter = InheritedPresenter.new('inherited', {:title => 'Bar'})
+    assert_equal 'Foobar', presenter.title
+  end
+  
+  test 'inherited presenter responds to #data' do
+    presenter = InheritedPresenter.new('inherited', {:data => ['Foobar']})
+    assert_equal ['Foobar'], presenter.data
+  end
+  
+  test 'inherited presenter returns the default value for #data' do
+    presenter = InheritedPresenter.new('inherited', {})
+    assert_equal [], presenter.data
   end
 end
